7c93a68b164d2c735400d9afb4b1a90ff7c24b2a
gabe <gpinkard@pugetsound.edu>
2017-02-20 18:33:10 -0800

    completed to 7.3.5

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -9,6 +9,15 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 var renderHomepage = function(req, res, responseBody) {
+  var message;
+  if (!(responseBody instanceof Array)) {
+    message = "API lookup error";
+    responseBody = [];
+  } else {
+    if (!responseBody.length) {
+      message = "No places found nearby";
+    }
+  }
   res.render('locations-list', {
     title: 'Loc8r - find a place to work with wifi',
        pageHeader: {
@@ -16,7 +25,8 @@ var renderHomepage = function(req, res, responseBody) {
          strapline: 'Find places to work with wifi near you!'
        },
        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake, or a pint? Let Loc8r help you find the place you're looking for.",
-       locations: responseBody
+       locations: responseBody,
+       message: message
   });
 };
 
@@ -28,64 +38,70 @@ module.exports.homelist = function(req, res) {
     method : "GET",
     json: {},
     qs : {
-      lng : -122.4832,
-      lat : 47.2635,
+      lng : -122.4835,
+      lat : 47.2639,
       maxDistance : 20
     }
   };
   request(
     requestOptions,
     function(err, response, body) {
-      renderHomepage(req, res, body);
+        var i, data;
+        data = body;
+        if (response.statusCode === 200 && data.length) {
+          for(i = 0; i<data.length; i++) {
+            data[i].distance = _formatDistance(data[i].distance);
+          }
+        }
+        renderHomepage(req, res, data);
     }
   );
 };
 
+var _formatDistance = function (distance) {
+  var numDistance, unit;
+  if (distance > 1) {
+    numDistance = parseFloat(distance).toFixed(1);
+    unit = 'km';
+  } else {
+    numDistance = parseInt(distance * 1000,10);
+    unit = 'm';
+  }
+  return numDistance + unit;
+};
+
+var renderDetailPage = function (req, res, locDetail) {
+     res.render('location-info', {
+     title: locDetail.name,
+     pageHeader: {title: locDetail.name},
+     sidebar: {
+         context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+         callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+     },
+     location: locDetail
+     });
+};
+
 /* GET 'Location info' page */
 module.exports.locationInfo = function(req, res) {
-     res.render('location-info', {
-        title: 'Oppenheimer Cafe',
-        pageHeader: {title: 'Oppenheimer Cafe'
-        },
-        sidebar: {
-            context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
-            callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
-        },
-        location: {
-            name: 'Oppenheimer Cafe',
-            address: '1500 N. Warner St. Tacoma, WA 98416',
-            rating: 3,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-            coords: {
-                lat: 47.2635, lng: -122.4832
-            },
-            openingTimes: [{
-                days: 'Monday - Friday',
-                opening: '7:00am',
-                closing: '7:00pm',
-                closed: false
-            }, {
-            	days: 'Saturday',
-            	opening: '8:00am',
-            	closing: '5:00pm',
-            	closed: false
-            }, {
-                days: 'Sunday',
-                closed: true
-            }],
-            reviews: [{
-                author: 'Simon Holmes',
-                rating: 5,
-                timestamp: '16 July 2013',
-                reviewText: 'What a great place. I can\'t say enough good things about it.'
-            }, {
-                author: 'Charlie Chaplin',
-                rating: 3,
-                timestamp: '16 June 2013',
-                reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
-            }]
-        }
-});
+  var requestOptions, path;
+  path = "/api/locations/" + req.params.locationid;
+  requestOptions = {
+    url : apiOptions.server + path,
+    method : "GET",
+    json : {}
+  };
+  request(
+    requestOptions,
+    function(err, response, body) {
+      var data = body;
+      data.coords = {
+        lng : body.coords[0],
+        lat : body.coords[1]
+      };
+      renderDetailPage(req, res, data);
+    }
+  );
 };
 
 /* GET 'Add review' page */
