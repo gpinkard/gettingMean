22b816b9244b65a66dadb836a2e0505269e718ff
gabe <gpinkard@pugetsound.edu>
2017-03-10 22:10:55 -0800

    Finished chapter 8

--- public/angular/loc8rApp.js
+++ public/angular/loc8rApp.js
@@ -32,25 +32,49 @@ var ratingStars = function () {
 };
 
 var loc8rData = function ($http) {
-  return $http.get('/api/locations?lng=-122.48&lat=47.26&maxDistance=20')
+  var locationByCoords = function (lat, lng) {
+    return $http.get('/api/locations?lng=' + lng + '&lat=' + lat + '&maxDistance=20');
+  };
+  return {
+    locationByCoords : locationByCoords
+  };
 };
 
 var locationListCtrl = function ($scope, loc8rData, geolocation) {
-  $scope.message = "Searching for nearby places";
-  loc8rData
-    .success(function(data) {
-      $scipe.message = data.length > 0 ? "" : "No locations found";
-      $scope.data = { locations: data };
-    })
-    .error(function (e) {
-      $scope.message = "Sorry, something's gone wrong ";
+  $scope.message = "Checking your location";
+
+  $scope.getData = function (position) {
+    var lat = position.coords.latitude, lng = position.coords.longitude;
+    $scope.message = "Searching for places nearby";
+    loc8rData.locationByCoords(lat, lng)
+      .success(function(data) {
+        $scope.message = data.length > 0 ? "" : "No locations found";
+        $scope.data = { locations: data };
+      })
+      .error(function (e) {
+        $scope.message = "Sorry, something's gone wrong";
+      });
+  };
+
+  $scope.showError = function(error) {
+    $scope.$apply(function() {
+      $scope.message = error.message;
     });
+  };
+
+  $scope.noGeo = function () {
+    $scope.$apply(function () {
+      $scope.message = "Geolocation not supported by this browser.";
+    });
+  };
+
+  geolocation.getPosition($scope.getData,$scope.showError,$scope.noGeo);
 };
 
 var geolocation = function () {
   var getPosition = function (cbSuccess, cbError, cbNoGeo) {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(cbSuccess, cdError);
+      navigator.geolocation.getCurrentPosition(cbSuccess, cbError);
     }
     else {
       cbNoGeo();
