3ae074a0a90212c8193ee55f9dcd3b39a142b73b
gabe <gpinkard@pugetsound.edu>
2017-03-11 16:19:24 -0800

    finished to 9.3.3, errors

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -6,3 +6,7 @@ module.exports.about = function (req, res) {
 		content: 'Loc8r was created to help peple find places to sit down and get a bit of work done\n\nNulla eleifend, augue vitae tempus ullamcorper, augue dui gravida urna, pulvinar gravida lectus nulla non arcu. Nulla laoreet lacus purus.'
 	});
 };
+
+module.exports.angularApp = function(req, res) {
+  res.render('layout', { title: 'Loc8r' });
+};
