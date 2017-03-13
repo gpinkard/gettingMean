78847602eb51b2bd6e363af240fbf52a6ab5636c
gabe <gpinkard@pugetsound.edu>
2017-03-12 21:50:14 -0700

    up to 10.1.3

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require('./app_api/models/db');
 var uglifyJs = require("uglify-js");
 var fs = require('fs');
 
-var routes = require('./app_server/routes/index');
+//var routes = require('./app_server/routes/index');
 var routesApi = require('./app_api/routes/index');
 var users = require('./app_server/routes/users');
 
@@ -23,7 +23,10 @@ var appClientFiles = [
   'app_client/common/services/geolocation.service.js',
   'app_client/common/services/loc8rData.service.js',
   'app_client/common/filters/formatDistance.filter.js',
-  'app_client/common/directives/ratingStars/ratingStars.directive.js'
+  'app_client/common/directives/ratingStars/ratingStars.directive.js',
+  'app_client/common/directives/footerGeneric/footerGeneric.directive.js',
+  'app_client/common/directives/navigation/navigation.directive.js',
+  'app_client/common/directives/pageHeader/pageHeader.directive.js'
 ];
 var uglified = uglifyJs.minify(appClientFiles, { compress : false });
 
@@ -44,10 +47,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'app_client')));
 
-app.use('/', routes);
+//app.use('/', routes);
 app.use('/api', routesApi);
 app.use('/users', users);
-
+app.use(function(req, res) {
+  res.sendfile(path.join(__dirname, 'app_client', 'index.html'));
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
